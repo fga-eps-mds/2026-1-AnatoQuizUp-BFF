f5068b64ab4322cba66671c95bf48f3c9c9f2445
marialuisa214 <maria.luisa.alves214@gmail.com>
2026-05-10 01:26:21 -0300

    feat: adiciona rota de questões professor

---
+++ src/routes/questoes.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+import { backendClient } from "@/shared/clients/backend.client";
+import { middlewareAutenticacao } from "@/shared/middlewares/autenticacao.middleware";
+import { criarProxyHandler } from "@/shared/middlewares/proxy.middleware";
+
+const router = Router();
+
+router.use(middlewareAutenticacao);
+router.all(/.*/, criarProxyHandler(backendClient));
+
+export { router as questoesRouter };
