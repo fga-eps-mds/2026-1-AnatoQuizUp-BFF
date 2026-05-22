5d7bdd2418c48b0503d0ef7f5fa67e559aa39e4a
pkbceira03 <cabeceira2003@gmail.com>
2026-05-22 20:55:58 -0300

    feat: adiciona endpoint de lista

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,7 @@ import { iaRouter } from "@/routes/ia.routes";
 import { questoesRouter } from "@/routes/questoes.routes";
 import { turmasRouter } from "@/routes/turmas.routes";
 import { usuariosRouter } from "@/routes/usuarios.routes";
+import { listasRouter } from "./lista.routes";
 
 import { quizClient } from "@/shared/clients/quiz.client";
 import { criarProxyHandler } from "@/shared/middlewares/proxy.middleware";
@@ -21,7 +22,7 @@ apiRouter.use("/questoes", questoesRouter);
 apiRouter.use("/ia", iaRouter);
 apiRouter.use("/turmas", turmasRouter);
 apiRouter.use("/usuarios", usuariosRouter);
-
 apiRouter.use("/quiz", middlewareAutenticacao, criarProxyHandler(quizClient));
+apiRouter.use("/lista", listasRouter)
 
 export { apiRouter };
